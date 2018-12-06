11580d0f4c9ed1c38ed532193e3343fbd67ab5a3
Félix-Antoine Fortin <felix.antoine.fortin@gmail.com>
2018-12-06 17:43:10 -0500

    Update flannel and helm version

--- init-master.bash
+++ init-master.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-kubeadm init --pod-network-cidr=10.244.0.0/16
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 
 # By now the master node should be ready!
 mkdir -p $HOME/.kube
@@ -9,15 +9,16 @@ cp --remove-destination /etc/kubernetes/admin.conf $HOME/.kube/config
 chown ${SUDO_UID} $HOME/.kube/config
 
 # Install flannel
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
+
 
 # Make master node a running worker node too!
 # FIXME: Use taint tolerations instead in the future
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
 # Install helm
-curl https://storage.googleapis.com/kubernetes-helm/helm-v2.8.0-linux-amd64.tar.gz | tar xvz
-mv linux-amd64/helm /usr/local/bin
+curl https://storage.googleapis.com/kubernetes-helm/helm-v2.11.0-linux-amd64.tar.gz | tar xvz
+sudo mv linux-amd64/helm /usr/local/bin
 rm -rf linux-amd64
 
 kubectl --namespace kube-system create sa tiller
