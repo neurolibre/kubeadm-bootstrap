3c05eb126196da7700bf489cab05ce25b5d7b2fa
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2019-03-26 09:54:49 -0400

    Remove support and support helm install from init-master

--- init-master.bash
+++ init-master.bash
@@ -28,7 +28,3 @@ kubectl --namespace=kube-system patch deployment tiller-deploy --type=json --pat
 
 # Wait for tiller to be ready!
 kubectl rollout status --namespace=kube-system deployment/tiller-deploy --watch
-
-# Install nginx and other support stuff!
-cd support && helm dep up && cd ..
-helm install --name=support --namespace=support support/
