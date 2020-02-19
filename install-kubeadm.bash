857a7152ce834ce017f634d2cb7ed449de071673
ltetrel <37963074+ltetrel@users.noreply.github.com>
2020-02-19 15:45:00 -0500

    Update install-kubeadm.bash
    updating kubernetes-cni

--- install-kubeadm.bash
+++ install-kubeadm.bash
@@ -21,7 +21,7 @@ systemctl start docker
 # Install kubernetes components!
 apt-get install -y kubernetes-cni=0.7.5-00
 apt-get install -y \
-        kubelet=1.16.0-00 \
-        kubeadm=1.16.0-00 \
-        kubectl=1.16.0-00
+        kubelet=1.17.0-00 \
+        kubeadm=1.17.0-00 \
+        kubectl=1.17.0-00
 
