3314f71f163fed106655218a0e27c56090afc0fe
ltetrel <37963074+ltetrel@users.noreply.github.com>
2020-02-20 12:04:05 -0500

    Update init-master.bash
    fixing acces permission for .kube

--- init-master.bash
+++ init-master.bash
@@ -8,8 +8,8 @@ sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 
 # To use the cluster
 mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+cp --remove-destination /etc/kubernetes/admin.conf $HOME/.kube/config
+chown ${SUDO_UID} $HOME/.kube/config
 
 # Install flannel
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/2140ac876ef134e0ed5af15c65e414cf26827915/Documentation/kube-flannel.yml
