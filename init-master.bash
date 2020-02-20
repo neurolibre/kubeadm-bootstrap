b0d40fc14ea2effe078dd8200ca417131aef2e96
ltetrel <37963074+ltetrel@users.noreply.github.com>
2020-02-20 11:44:50 -0500

    Update init-master.bash
    Changed kube-flannel version and other stuff (https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/)

--- init-master.bash
+++ init-master.bash
@@ -1,15 +1,18 @@
 #!/bin/bash
 set -e
 
+# By now the master node should be ready!
+# Install flannel
+sysctl net.bridge.bridge-nf-call-iptables=1
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/2140ac876ef134e0ed5af15c65e414cf26827915/Documentation/kube-flannel.yml
+
+# Initialize 
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 
-# By now the master node should be ready!
+# To use the cluster
 mkdir -p $HOME/.kube
-cp --remove-destination /etc/kubernetes/admin.conf $HOME/.kube/config
-chown ${SUDO_UID} $HOME/.kube/config
-
-# Install flannel
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 # Make master node a running worker node too!
 # FIXME: Use taint tolerations instead in the future
