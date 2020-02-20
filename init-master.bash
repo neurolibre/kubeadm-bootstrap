ba88511d9595ae4640b738f83af4ca7ce8ff68a9
ltetrel <37963074+ltetrel@users.noreply.github.com>
2020-02-20 13:52:56 -0500

    Update init-master.bash
    updating helm install following jupyterhub tutorial.. 
    https://binderhub.readthedocs.io/en/latest/create-cloud-resources.html#install-helm

--- init-master.bash
+++ init-master.bash
@@ -18,15 +18,12 @@ kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/2140ac876ef134
 # FIXME: Use taint tolerations instead in the future
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
-# Install helm
-curl https://storage.googleapis.com/kubernetes-helm/helm-v2.14.0-linux-amd64.tar.gz | tar xvz
-mv linux-amd64/helm /usr/local/bin
-rm -rf linux-amd64
-
+# Install and initialize helm
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
 kubectl --namespace kube-system create serviceaccount tiller
 kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
-helm init --service-account tiller --override spec.selector.matchLabels.'name'='tiller',spec.selector.matchLabels.'app'='helm' --output yaml | sed 's@apiVersion: extensions/v1beta1@apiVersion: apps/v1@' | kubectl apply -f -
-kubectl --namespace=kube-system patch deployment tiller-deploy --type=json --patch='[{"op": "add", "path": "/spec/template/spec/containers/0/command", "value": ["/tiller", "--listen=localhost:44134"]}]'
+helm init --service-account tiller --wait
+kubectl patch deployment tiller-deploy --namespace=kube-system --type=json --patch='[{"op": "add", "path": "/spec/template/spec/containers/0/command", "value": ["/tiller", "--listen=localhost:44134"]}]'
 
 # Wait for tiller to be ready!
 kubectl rollout status --namespace=kube-system deployment/tiller-deploy --watch
