f450b639e30519a5ae810b3ad83a7fe45d9ac4e9
Loic Tetrel <37963074+ltetrel@users.noreply.github.com>
2020-06-26 12:38:35 -0400

    Update init-master.bash
    (re) adding master as a worker (to make cert-manager work)

--- init-master.bash
+++ init-master.bash
@@ -14,6 +14,10 @@ chown ${SUDO_UID} $HOME/.kube/config
 # Install flannel
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/2140ac876ef134e0ed5af15c65e414cf26827915/Documentation/kube-flannel.yml
 
+# Make master node a running worker node too!
+# FIXME: Use taint tolerations instead in the future
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
 # Install and initialize helm
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
 kubectl --namespace kube-system create serviceaccount tiller
